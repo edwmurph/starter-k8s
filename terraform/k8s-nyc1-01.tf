4d6b4831af48f9dc2b18b1d746a4b6bccb543970
edwmurph <edwmurph3@gmail.com>
2020-09-19 12:10:40 -0400

    update default node pool

--- terraform/k8s-nyc1-01.tf
+++ terraform/k8s-nyc1-01.tf
@@ -4,8 +4,11 @@ resource "digitalocean_kubernetes_cluster" "personal" {
   version = "1.18.8-do.0" # Latest slug from `doctl kubernetes options versions`
 
   node_pool {
-    name       = "pool-01"
+    name       = "default-pool"
     size       = "s-1vcpu-2gb" # Options from `doctl kubernetes options sizes`
     node_count = 1
+    auto_scale = true
+    min_nodes = 1
+    max_nodes = 2
   }
 }
