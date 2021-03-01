54b81e31e6f6502724695da85eb256381a2e41e0
edwmurph <edwmurph3@gmail.com>
2021-03-01 09:10:30 -0500

    upgrade k8s

--- terraform/personal.tf
+++ terraform/personal.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "personal" {
   name    = "personal"
   region  = "nyc1"
-  version = "1.18.8-do.0" # Latest slug from `doctl kubernetes options versions`
+  version = "1.20.2-do.0" # Latest slug from `doctl kubernetes options versions`
 
   node_pool {
     name       = "default-pool"
